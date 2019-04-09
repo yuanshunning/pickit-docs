4a28c8a8887693328c5d1742453929b86b198ed7
Filip Vanderstappen <filip.vanderstappen@pickit3d.com>
2019-04-09 15:19:06 +0200

    Added custom css for Pick-it

--- docs/conf.py
+++ docs/conf.py
@@ -270,3 +270,6 @@ htmlhelp_basename = 'PickitDocs'
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
